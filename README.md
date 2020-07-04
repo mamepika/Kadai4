a299536ac01f17813be650862bc66a75204410c8
tatsushi sato <a1918ts@aiit.ac.jp>
2020-07-04 17:36:20 +0900

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# システムソフトウェア特論 課題4
+
+## kadai4.sh
+2つの引数の最大公約数を表示するシェルスクリプト
+
+## kadai4_test.sh
+kadai4.shをテストするテスト用シェルスクリプト
+
+## .travis.yml
+Travis CI用のymlファイル
---
+++ kadai4_test.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+
+result=$(./kadai4.sh 64 16)
+if [ $result = 16 ] ; then
+	echo "test1 OK"
+else
+	echo "error in test1"
+	exit 1
+fi
+
+result=$(./kadai4.sh 32 12)
+if [ $result = 4 ] ; then
+	echo "test2 OK!"
+	exit 0
+else
+	echo "error in test2"
+fi
+
+
